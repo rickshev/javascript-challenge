ab55ed6fa688413b8eaf31860032e554f91372d1
rickshev <rick.shevlin@gmail.com>
2020-06-02 13:45:37 -0400

    Modified app.js code to work on input challenge. Added images to repository.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -7,8 +7,27 @@ tbody = d3.select("tbody");
 tableData.forEach(function(ufoReport) {
     var row = tbody.append("tr");
     Object.entries(ufoReport).forEach(function([key, value]) {
-        console.log(key, value);
+        // console.log(key, value);
         var cell = row.append("td");
         cell.text(value);
     });
 });
+
+var button = d3.select("#filter-btn");
+var form = d3.select("#form");
+
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+function runEnter() {
+    d3.preventDefault();
+
+    var inputElement = d3.select("#datetime");
+
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+    console.log(tableData);
+
+    var filteredUfo = tableData.filter(data => data.datetime === inputValue);
+    console.log(filteredUfo);
+}
