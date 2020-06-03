6b96fbdaf6f7e75af0e0680bea408b0382c2d4e2
rickshev <rick.shevlin@gmail.com>
2020-06-03 14:08:53 -0400

    Added code to read input and modify data table.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,30 +4,40 @@ var tableData = data;
 // YOUR CODE HERE!
 tbody = d3.select("tbody");
 
-tableData.forEach(function(ufoReport) {
-    var row = tbody.append("tr");
-    Object.entries(ufoReport).forEach(function([key, value]) {
-        // console.log(key, value);
-        var cell = row.append("td");
-        cell.text(value);
+// tableData.forEach(function(ufoReport) {
+//     var row = tbody.append("tr");
+//     Object.entries(ufoReport).forEach(function([key, value]) {
+//         // console.log(key, value);
+//         var cell = row.append("td");
+//         cell.text(value);
+//     });
+// });
+
+function fillTable(data) {
+    tbody.text("");
+    data.forEach(function(ufo) {
+        row = tbody.append("tr");
+        Object.entries(ufo).forEach(function([key, value]) {
+            var cell = row.append("td").text(value);
+        });
     });
-});
+};
+
+fillTable(tableData);
 
 var button = d3.select("#filter-btn");
-var form = d3.select("#form");
+var inputElement = d3.select("#datetime");
 
-button.on("click", runEnter);
-form.on("submit", runEnter);
 
 function runEnter() {
-    d3.preventDefault();
-
-    var inputElement = d3.select("#datetime");
+    d3.event.preventDefault();
 
     var inputValue = inputElement.property("value");
     console.log(inputValue);
-    console.log(tableData);
 
     var filteredUfo = tableData.filter(data => data.datetime === inputValue);
-    console.log(filteredUfo);
-}
+    
+    fillTable(filteredUfo);
+};
+
+inputElement.on("change", runEnter);
